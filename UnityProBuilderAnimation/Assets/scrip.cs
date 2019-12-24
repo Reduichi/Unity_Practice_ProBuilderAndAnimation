ab72e711d13864c10dbf809236e807c0726165af
Reduichi <57396815+Reduichi@users.noreply.github.com>
2019-12-24 21:41:11 +0800

    金幣

---
+++ UnityProBuilderAnimation/Assets/scrip.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scrip : MonoBehaviour
+{
+    [Header("金幣")]
+    public GameObject coin;
+    [Header("金幣按鈕")]
+    public Button btnGetcoin;
+
+    public AudioSource aud;
+    public AudioClip Soundcoin;
+
+    public void Coin()
+    {
+        aud.PlayOneShot(Soundcoin);  // 播放音效
+        Quaternion rot = new Quaternion(0, 0, 0, 0);  // 旋轉
+        Vector3 pos = new Vector3(0, 0.5f, 0);  // 位置
+        Instantiate(coin, pos, rot); // 生出金幣
+    }
+}
